ad2b8838b86c3756b28104db2e30b45083e149de
iwebnal <iwebnal@gmail.com>
2020-04-06 09:01:31 +0400

    first commit

---
+++ src/Pages/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class About extends Component {
+    render() {
+        return(
+            <div style={{marginTop:'65px'}}>
+                Hello About!
+            </div>
+        )
+    }
+}
---
+++ src/Pages/Blog.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Blog extends Component {
+    render() {
+        return(
+            <div style={{marginTop:'65px'}}>
+                Hello Blog!
+            </div>
+        )
+    }
+}
